0bdaf7763e632f3f819a2659817936f66baab1d4
yanchao7511461 <yanchao7511461@gmail.com>
2021-03-12 20:17:37 +0800

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,6 +1,6 @@
 # -*- coding: UTF-8 -*-
 import requests as req
-import json,sys,time
+import json,sys,time,random
 #先注册azure应用,确保应用有以下权限:
 #files:	Files.Read.All、Files.ReadWrite.All、Sites.Read.All、Sites.ReadWrite.All
 #user:	User.Read.All、User.ReadWrite.All、Directory.Read.All、Directory.ReadWrite.All
@@ -32,11 +32,14 @@ def gettoken(refresh_token):
         f.write(refresh_token)
     return access_token
 def main():
+    localtime = time.asctime( time.localtime(time.time()) )
+    wait_sec = random.randint(10,4*60)
+    print("wait sec-- "+str(wait_sec))
+    time.sleep(wait_sec)
     fo = open(path, "r+")
     refresh_token = fo.read()
     fo.close()
     global num1
-    localtime = time.asctime( time.localtime(time.time()) )
     access_token=gettoken(refresh_token)
     headers={
     'Authorization':access_token,
@@ -46,36 +49,47 @@ def main():
         if req.get(r'https://graph.microsoft.com/v1.0/me/drive/root',headers=headers).status_code == 200:
             num1+=1
             print("1调用成功"+str(num1)+'次')
+            time.sleep(random.randint(1,20))
         if req.get(r'https://graph.microsoft.com/v1.0/me/drive',headers=headers).status_code == 200:
             num1+=1
             print("2调用成功"+str(num1)+'次')
+            time.sleep(random.randint(1,20))
         if req.get(r'https://graph.microsoft.com/v1.0/drive/root',headers=headers).status_code == 200:
             num1+=1
             print('3调用成功'+str(num1)+'次')
+            time.sleep(random.randint(1,20))
         if req.get(r'https://graph.microsoft.com/v1.0/users ',headers=headers).status_code == 200:
             num1+=1
             print('4调用成功'+str(num1)+'次')
+            time.sleep(random.randint(1,20))
         if req.get(r'https://graph.microsoft.com/v1.0/me/messages',headers=headers).status_code == 200:
             num1+=1
-            print('5调用成功'+str(num1)+'次')    
+            print('5调用成功'+str(num1)+'次') 
+            time.sleep(random.randint(1,20))
         if req.get(r'https://graph.microsoft.com/v1.0/me/mailFolders/inbox/messageRules',headers=headers).status_code == 200:
             num1+=1
-            print('6调用成功'+str(num1)+'次')    
+            print('6调用成功'+str(num1)+'次')
+            time.sleep(random.randint(1,20))
         if req.get(r'https://graph.microsoft.com/v1.0/me/mailFolders/Inbox/messages/delta',headers=headers).status_code == 200:
             num1+=1
             print('7调用成功'+str(num1)+'次')
+            time.sleep(random.randint(1,20))
         if req.get(r'https://graph.microsoft.com/v1.0/me/drive/root/children',headers=headers).status_code == 200:
             num1+=1
             print('8调用成功'+str(num1)+'次')
+            time.sleep(random.randint(1,20))
         if req.get(r'https://api.powerbi.com/v1.0/myorg/apps',headers=headers).status_code == 200:
             num1+=1
-            print('8调用成功'+str(num1)+'次') 
+            print('8调用成功'+str(num1)+'次')
+            time.sleep(random.randint(1,20))
         if req.get(r'https://graph.microsoft.com/v1.0/me/mailFolders',headers=headers).status_code == 200:
             num1+=1
             print('9调用成功'+str(num1)+'次')
+            time.sleep(random.randint(1,20))
         if req.get(r'https://graph.microsoft.com/v1.0/me/outlook/masterCategories',headers=headers).status_code == 200:
             num1+=1
             print('10调用成功'+str(num1)+'次')
+            time.sleep(random.randint(1,20))
             print('此次运行结束时间为 :', localtime)
     except:
         print("pass")
