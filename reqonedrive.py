e61c9f09b0e2ab7b598692d82bed9399cdc18c52
yanchao7511461 <yanchao7511461@gmail.com>
2021-06-08 13:20:45 +0800

    Create reqonedrive.py

---
+++ reqonedrive.py
@@ -0,0 +1,25 @@
+import requests as req
+import time
+
+def r():
+    req.get("https://onedrive.thresh.workers.dev/dndm/")
+    req.get("https://onedrive.thresh.workers.dev/instaloader/")
+    req.get("https://onedrive.thresh.workers.dev/telegram/videos/")
+    req.get("https://onedrive.thresh.workers.dev/instaloader/dorosiwa/")
+    req.get("https://onedrive.thresh.workers.dev/instaloader/bikini_kor/")
+    req.get("https://onedrive.thresh.workers.dev/dndm/[22sht.me]pppd-481-C/pppd-481-C.mp4")
+    req.get("https://onedrive.thresh.workers.dev/dndm/[JAV]%20[Uncensored]%20FC2%20PPV%201228911%20[1080p]/FC2-PPV-1228911.mp4")
+
+    req.get("https://onedrive.thresh.workers.dev/dndm/")
+    req.get("https://onedrive.thresh.workers.dev/instaloader/")
+    req.get("https://onedrive.thresh.workers.dev/telegram/videos/")
+    req.get("https://onedrive.thresh.workers.dev/instaloader/dorosiwa/")
+    req.get("https://onedrive.thresh.workers.dev/instaloader/bikini_kor/")
+    req.get("https://onedrive.thresh.workers.dev/dndm/[22sht.me]pppd-481-C/pppd-481-C.mp4")
+    req.get("https://onedrive.thresh.workers.dev/dndm/[JAV]%20[Uncensored]%20FC2%20PPV%201228911%20[1080p]/FC2-PPV-1228911.mp4")
+
+if __name__ == "__main__":
+  try:
+    r()
+  except Exception as e:
+    print('request error')
