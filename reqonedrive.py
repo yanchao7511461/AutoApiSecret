d83105113d15d41b79cda48c2a1ee6d98c22e0fe
yanchao7511461 <yanchao7511461@gmail.com>
2021-06-08 13:26:58 +0800

    Update reqonedrive.py

--- reqonedrive.py
+++ reqonedrive.py
@@ -3,20 +3,34 @@ import time
 
 def r():
     req.get("https://onedrive.thresh.workers.dev/dndm/")
+    print("req1")
     req.get("https://onedrive.thresh.workers.dev/instaloader/")
+    print("req2")
     req.get("https://onedrive.thresh.workers.dev/telegram/videos/")
+    print("req3")
     req.get("https://onedrive.thresh.workers.dev/instaloader/dorosiwa/")
+    print("req4")
     req.get("https://onedrive.thresh.workers.dev/instaloader/bikini_kor/")
+    print("req5")
     req.get("https://onedrive.thresh.workers.dev/dndm/[22sht.me]pppd-481-C/pppd-481-C.mp4")
+    print("req6")
     req.get("https://onedrive.thresh.workers.dev/dndm/[JAV]%20[Uncensored]%20FC2%20PPV%201228911%20[1080p]/FC2-PPV-1228911.mp4")
+    print("req7")
 
     req.get("https://onedrive.thresh.workers.dev/dndm/")
+    print("req8")
     req.get("https://onedrive.thresh.workers.dev/instaloader/")
+    print("req9")
     req.get("https://onedrive.thresh.workers.dev/telegram/videos/")
+    print("req10")
     req.get("https://onedrive.thresh.workers.dev/instaloader/dorosiwa/")
+    print("req11")
     req.get("https://onedrive.thresh.workers.dev/instaloader/bikini_kor/")
+    print("req12")
     req.get("https://onedrive.thresh.workers.dev/dndm/[22sht.me]pppd-481-C/pppd-481-C.mp4")
+    print("req13")
     req.get("https://onedrive.thresh.workers.dev/dndm/[JAV]%20[Uncensored]%20FC2%20PPV%201228911%20[1080p]/FC2-PPV-1228911.mp4")
+    print("req14")
 
 if __name__ == "__main__":
   try:
